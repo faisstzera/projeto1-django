859702b191c68b36a971a6f70c191f8b5da69b0b
faisst <alexandre.faisst@gmail.com>
2022-10-25 15:03:10 -0300

    Adicionar Models

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
 ]
