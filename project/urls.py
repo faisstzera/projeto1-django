436d94bc85738f9cedd10ac56d45d53320c4d7ec
faisst <alexandre.faisst@gmail.com>
2022-10-26 14:16:10 -0300

    Mudar arquivos

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""projeto URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -24,5 +24,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- recipes/admin.py
+++ recipes/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 from .models import Category, Recipe
 
-# Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    ...
 
+
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    ...
+
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Recipe, RecipeAdmin)
